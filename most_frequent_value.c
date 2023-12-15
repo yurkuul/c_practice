ae53d0c701ca896376401ba16fd2a21a86a12c7d
LZ-FSDev <95185447+yurkuul@users.noreply.github.com>
2023-12-15 11:04:12 -0600

    Update most_frequent_value.c

--- most_frequent_value.c
+++ most_frequent_value.c
@@ -25,22 +25,48 @@ const int MAX_VAL = 100;
 int most_frequent_val(int arr[], int len) {
     int freq_len = abs(MIN_VAL) + abs(MAX_VAL) + 1;
     int freqs[freq_len];
+    int most_freq = 0;
 
     int index = 0;
     for (int i = MIN_VAL; i < MAX_VAL+1; i++) {
-        freqs[index] = i;
-        printf("%d\n", freqs[index]);
+        freqs[index] = 0;
         index++;
     }
 
-    return 1;
+    for (int i = 0; i < len; i++) {
+        if (arr[i] >= MIN_VAL && arr[i] <= MAX_VAL) {
+            if (arr[i] < 0) { //negative value
+                freqs[abs(arr[i])]++;
+            } else if (arr[i] > 0) { //positive value
+                freqs[arr[i] + 100]++;
+            }
+        }
+    }
+
+    for (int i = 0; i < freq_len; i++) {
+        if (most_freq < freqs[i]) {
+            most_freq = freqs[i];
+        }
+    }
+
+    return most_freq;
 }
 
 int main() {
     int arr[] = {0, -7, 102, -7, 5, 5,  -7, 3, 102, 102, -5, -5, 16, -73, 102, 0, -73, 8, 8, 8};
     int len = 20;
 
-    most_frequent_val(arr, len);
+    int num = most_frequent_val(arr, len);
+
+    printf("Array: \n[");
+
+    for (int i = 0; i < len-1; i++) {
+        printf("%d, ", arr[i]);
+    }
+
+    printf("%d]\n", arr[len-1]);
+
+    printf("Most frequent appearance of a number: %d\n", num);
 
     return 1;
 }
